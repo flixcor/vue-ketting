b7352da7816bd247870461e13a1afe8b101c873e
Felix Cornelissen <felixcornelissen@gmail.com>
2021-06-25 22:43:53 +0200

    cleanup

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,2 @@
 import { Resource } from 'ketting';
-import { shallowRef, readonly } from 'vue';
-
-const readonlyRef = 'readonlyRef'
-
-class Wrapper<T> {
-    // wrapped has no explicit return type so we can infer it
-    [readonlyRef](e: T) {
-        return readonly(shallowRef(e))
-    }
-}
-
-export type ReadonlyRef<T> = ReturnType<Wrapper<T>[typeof readonlyRef]>
 export type ResourceLike<T> = Resource<T> | PromiseLike<Resource<T>> | string;

fa94e7617f6025ac3b0fc8fd3bf4cfd24e0fb975
Felix Cornelissen <felixcornelissen@gmail.com>
2021-06-28 10:31:39 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-dts'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    dts()
+    vue()
   ],
   build: {
     lib: {
